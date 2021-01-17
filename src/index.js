fc04553f0d1f6efd9a379da819706ddc46fe089f
VanWilder199 <72692962+VanWilder199@users.noreply.github.com>
2021-01-17 15:29:18 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const { neq } = require("semver");
+
 module.exports = function reverse (n) {
-  
+  const nValue = Math.abs(n);
+  const nStr = nValue.toString();
+  const nReverse = nStr.split('').reverse().join('');
+ 
+  return nReverse
 }
